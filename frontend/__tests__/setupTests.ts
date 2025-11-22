8ff4cfab8101508eedcebe4bce06b3a9168773f5
Leo95995 <malvolti68@gmail.com>
2025-11-22 23:47:21 +0100

    tests(rtl): add setup for react testing library

--- frontend/__tests__/setupTests.ts
+++ frontend/__tests__/setupTests.ts
@@ -1,6 +1,46 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
 import { jest } from '@jest/globals';
+if (!global.TextEncoder) {
+  // @ts-ignore
+  global.TextEncoder = TextEncoder;
+}
+// @ts-ignore
+global.TextDecoder = TextDecoder;
 
 // fetch mock globale con tipi corretti
 const mock = jest.fn() as jest.Mock;
 global.fetch = mock as unknown as typeof fetch;
+
+
+// mocko i valori che non posso avere
+// setupTests.ts
+
+
+type ModeType = "test"| "production" | "development"
+
+
+jest.doMock("../src/utils/environment", () => {
+  const backendUrl = "http://localhost:3000"; // valore di test
+  let mode: ModeType = "test"; 
+  return {
+    environment: {
+      backend_url: backendUrl,
+    },
+    services_url: {
+      nasa: backendUrl + "/nasa",
+      github: backendUrl + "/github",
+      crypto: backendUrl + "/crypto",
+      weather: backendUrl + "/weather",
+      PROXY_ENDPOINT: "/proxy-image",
+    },
+    AppMode: {
+      getMode: () => mode,
+      isProduction: () => mode === "production",
+      isDevelopment: () => mode === "development",
+      _setMode: (m: ModeType) => {
+        mode = m;
+      },
+    },
+  };
+});
--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,18 +1,16 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',         
-  // Mi serve per setuppare un ambiente simile al browser     
-  testEnvironment: 'jsdom',       
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setupTests.ts'], // setup globale RTL e mock
-  globals: {
-  'ts-jest': {
-    tsconfig: 'tsconfig.test.json'
-  }
-},
-  roots: ['<rootDir>/__tests__/components', '<rootDir>/__tests__/pages', '<rootDir>/__tests__/pages'], // test include i miei test. src -> include il mio codice
-  moduleNameMapper: {            
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
+  },
+  roots: ['<rootDir>/__tests__/components', '<rootDir>/__tests__/utils', '<rootDir>/__tests__/pages', '<rootDir>/__tests__/smoke'],
+  moduleNameMapper: {
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
+      '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js' // trasforma tutti i file di quel tipo in stringhe
   },
 };
 
